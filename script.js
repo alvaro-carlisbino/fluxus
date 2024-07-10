b1c104a4a0b66973780f47931cad8e8b4ccfbce1
Bryzzen <xdbrgames10@gmail.com>
2024-07-09 21:42:19 -0300

    scroll wheel

--- script.js
+++ script.js
@@ -86,3 +86,13 @@ const slide = (dir) => {
 		}
 	}
 };
+
+document.addEventListener('wheel', function(event) {
+	if (event.deltaY < 0) {
+		// Rolagem para cima
+		slide(-1);
+	} else {
+		// Rolagem para baixo
+		slide(1);
+	}
+});
